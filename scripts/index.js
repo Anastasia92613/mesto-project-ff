956f39807809af9f08a371e5724f7110373e5554
Anastasia Vasiliadi <92613@mail.ru>
2025-05-28 08:18:18 +0300

    Исправления по итогам ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,24 @@
 const container = document.querySelector('.places');
 const cardsContainer = container.querySelector('.places__list');
 
-function addCard(link, name) {
+function createCard(link, name, deleteCard) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  const buttonsDelete = cardElement.querySelectorAll('.card__delete-button');
+  const buttonDelete = cardElement.querySelector('.card__delete-button');
 
   cardElement.querySelector('.card__image').setAttribute('src', link);
   cardElement.querySelector('.card__image').setAttribute('alt', name);
   cardElement.querySelector('.card__title').textContent = name;
+  
+  buttonDelete.addEventListener('click',function (evt) { 
+    const deletedCard = evt.target.closest('.card');
+    deleteCard(deletedCard);
+   });
 
-  cardsContainer.append(cardElement);
-
-    buttonsDelete.forEach((button) => {
-      button.addEventListener('click', function (evt) {
-        const deletedCard = evt.target.closest('.card');
-        deleteCard(deletedCard);
-      });
-    });
+  return cardElement;
 };
 
-initialCards.forEach(card =>{addCard(card.link, card.name)});
+initialCards.forEach(card =>{cardsContainer.append(createCard(card.link, card.name, deleteCard))});
 
 function deleteCard (card) {
  card.remove();
