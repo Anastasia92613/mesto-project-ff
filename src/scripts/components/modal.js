bee368e36a599b89f2279dd040af89addc524a74
Anastasia Vasiliadi <92613@mail.ru>
2025-07-01 14:53:46 +0300

    Еще одно исправление

--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -1,30 +1,35 @@
+let currentContainer;
+let currentButtonClose;
 
-  function closePopup(container, buttonClose) {
-    container.classList.remove('popup_is-opened');
-    container.classList.remove('popup_is-animated');
-    document.removeEventListener('keydown', closePopupBuEsc); 
-    document.removeEventListener('click', closePopupByOverlay);
-    buttonClose.removeEventListener('click', closePopup);
+function closePopup() {
+  currentContainer.classList.remove('popup_is-opened');
+  currentContainer.classList.remove('popup_is-animated');
+  document.removeEventListener('keydown', closePopupBuEsc); 
+  document.removeEventListener('click', closePopupByOverlay);
+  currentButtonClose.removeEventListener('click', closePopup);
+};
+
+function closePopupBuEsc (evt) {
+  if (evt.key === 'Escape') {
+    closePopup();
   };
+};
 
-    function closePopupBuEsc (evt, container, buttonClose) {
-    if (evt.key === 'Escape') {
-      closePopup(container, buttonClose);
-    };
+function closePopupByOverlay(evt) {
+  if (evt.target === currentContainer) {
+    closePopup();
   };
-
-    function closePopupByOverlay(evt, container, buttonClose) {
-    if (evt.target === container) {
-      closePopup(container, buttonClose);
-    };
-  };
-
+};
 
 //Открытие и закрытие попапа
 export function handlePopup (container, buttonClose) {
-  container.classList.toggle('popup_is-opened');
-  container.classList.toggle('popup_is-animated');
-  buttonClose.addEventListener('click', () => closePopup(container, buttonClose));
-  document.addEventListener('keydown', (evt) =>  closePopupBuEsc (evt, container, buttonClose));
-  document.addEventListener('click', (evt) => closePopupByOverlay (evt, container, buttonClose));
+  
+  currentContainer = container;
+  currentButtonClose = buttonClose;
+
+  currentContainer.classList.toggle('popup_is-opened');
+  currentContainer.classList.toggle('popup_is-animated');
+  currentButtonClose.addEventListener('click', closePopup);
+  document.addEventListener('keydown', closePopupBuEsc);
+  document.addEventListener('click', closePopupByOverlay);
 }
